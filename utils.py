b9b56c2b21171bc69f6ba3f8cbf2cf19a3e71daa
Luke A Sargen <lukeasargen@gmail.com>
2022-03-11 20:33:26 -0500

    Fix wp dist to only check during auto mode

--- utils.py
+++ utils.py
@@ -1,5 +1,52 @@
+import os
+import glob
+
+import pandas as pd
 import numpy as np
 from scipy import stats
+
+
+def extract_data(log_dir, log_filename):
+
+    csv_filename = lambda k: os.path.join(log_dir, log_filename, k+".csv")
+    
+    with open(log_dir+log_filename+".log", "r") as filehandle:
+        log_list = filehandle.readlines()
+    os.makedirs(log_dir+log_filename, exist_ok=True)
+
+    # First find all the FMT messages and make empty arrays
+    fmt_list = [i for i in log_list if "FMT," in i]
+    save_data = {}
+    print("Number of fmt:", len(fmt_list))
+    for line in fmt_list:
+        s = line.split(", ")
+        save_data[s[3]] = [s[-1]]
+
+    # Go through each line and make add each csv line
+    for line in log_list:
+        # if "FMT," not in line:
+        idx = line.find(',')
+        save_data[line[:idx]].append(line[idx+2:])
+
+    # Save everything
+    for k in save_data.keys():
+        with open(csv_filename(k), "w+") as f:
+            f.writelines(save_data[k])
+
+    # Load dataframes and convert to data types
+    data = {}
+    for csv_path in glob.glob(csv_filename("*")):
+        k = os.path.splitext(os.path.basename(csv_path))[0]
+        if k=="FMT" or k=="MSG": continue  # this has extra commas
+        data[k] = pd.read_csv(csv_filename(k), delimiter=",")
+        try:
+            data[k] = data[k].apply(pd.to_numeric)
+        except:
+            print("No numerics:", k)
+    
+    data["PARM"]["Name"] = data["PARM"]["Name"].astype('string')
+    data["MODE"]["Mode"] = data["MODE"]["Mode"].astype('string')
+    return data
 
 
 def remove_outlier_rows(df, col, z_max=3):
@@ -25,3 +72,7 @@ def find_closest_row(value, df, colname, use_next=False):
 
 def slice_between(df, col, low, high):
     return df[df[col].between(low, high)].copy()
+
+def get_param(df, name, time=0):
+    param_df = df["PARM"][df["PARM"]["Name"]==" "+name]
+    return float(find_closest_row(time, param_df, "TimeUS")["Value"])
