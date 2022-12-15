35f3f4639537108a8740ad3a1aa98877e8d4f22b
Luke A Sargen <lukeasargen@gmail.com>
2022-12-15 12:09:41 -0500

    Update the cam params

--- tiles_video.py
+++ tiles_video.py
@@ -1,17 +1,13 @@
 # http://jepsonsblog.blogspot.tw/2012/11/rotation-in-3d-using-opencvs.html
 
-
+import sys
+import re
 import cv2
 import numpy as np
 
 TILE_SIZE = 256  # pixels
 EARTH_CIRCUMFERENCE = 40075.016686 * 1000  # in meters, at the equator
 EARTH_RADIUS_M = 6371000
-def cosd(x):
-    return np.cos(np.radians(x))
-
-def sind(x):
-    return np.sin(np.radians(x))
 
 def resolution(zoom, lat=0):
     m_per_pix = ((EARTH_CIRCUMFERENCE / TILE_SIZE) * np.cos(np.radians(lat))) / (2**zoom)
@@ -32,25 +28,31 @@ def ned_translate(lat, lon, dN=0, dE=0):
     return (newlat, newlon)
 
 class SimCam:
-    def __init__(self, mx, my, zoom, map_filename, image_size,
-            cam_att, focal_length, optical_center, distortion):
-        self.mx = mx
-        self.my = my
-        self.zoom = zoom
+    def __init__(self, map_filename, cam_params, cam_att):
         self.map_filename = map_filename
-        tl_lat, tl_lon = inv_mercator(mx, my, zoom)
-        self.tl_lat = tl_lat
-        self.tl_lon = tl_lon
-        m_per_pix = resolution(zoom=zoom, lat=tl_lat)
-        self.m_per_pix = m_per_pix
+        self.mx = int(re.search('swx(.*)-swy', map_filename).group(1))
+        self.my = int(re.search('ney(.*)-', map_filename).group(1))
+        self.zoom = int(re.search('z(.*).png', map_filename).group(1))
+        self.tl_lat, self.tl_lon = inv_mercator(self.mx, self.my, self.zoom)
+        self.m_per_pix = resolution(zoom=self.zoom, lat=self.tl_lat)
         self.ground_image = cv2.imread(self.map_filename)
-        # Replace the top corner with black to remap a "blank" pixel
-        self.ground_image[0][0] = np.zeros((1,3))
-        self.image_size = image_size
+        self.cam_params = cam_params
         self.cam_att = cam_att
-        self.focal_length = focal_length
-        self.optical_center = optical_center
-        self.distortion = distortion
+        self.dst_pixels, self.invalid_pixels = self.create_distorted_pixels()
+        
+    def create_distorted_pixels(self):
+        # Find the positive hash of the camera params
+        cam_params_hash = hash(str(self.cam_params)) & sys.maxsize
+        # Load the pixels if they exist or calculate them
+        distortion = self.cam_params["distortion"]
+        # if np.sum(abs(distortion))>0:
+        #     k1, k2, p1, p2, k3 = distortion
+        # else:
+        width, height = self.cam_params["width"], self.cam_params["height"]
+        dst_pixels = np.ones((height, width, 3), np.float32)
+        dst_pixels[:, :, :2] = np.mgrid[0:width, 0:height].T
+        invalid_pixels = np.full((height, width,), False)
+        return dst_pixels, invalid_pixels
 
     def lla_to_pixel(self, lat, lon, alt):
         """ Convert (lat, lon, alt) to (u,v,w) pixels"""
@@ -60,22 +62,10 @@ class SimCam:
         dE = dLon*(EARTH_RADIUS_M*np.cos(np.radians(self.tl_lat)))
         return [dE/self.m_per_pix, -dN/self.m_per_pix, alt/self.m_per_pix]
 
-    def capture(self, uav_pos, uav_att):
+    def make_extrinsics(self, uav_pos, uav_att):
         h, w, c = self.ground_image.shape
-        width, height = self.image_size
-        fx, fy = self.focal_length
-        cx, cy = self.optical_center
-
-        # Not used, but useful formulas for total FOV angle
-        # fov_x = np.degrees(2*np.arctan(0.5*width/fx))
-        # fov_y = np.degrees(2*np.arctan(0.5*height/fy))
-        # print(f"{fov_x = }. {fov_y = }")
-
-        # camera extrinsics
         p = self.lla_to_pixel(*uav_pos)
-        # print("uav pos pixel :", p)
         t = [w/2-p[0], h/2-p[1], p[2]]
-        # print(f"{t = }")
         T = np.eye(4)
         T[:3,3] = t
 
@@ -105,6 +95,8 @@ class SimCam:
                         [0, 0, 1]])
 
         # Projection 3D -> 2D matrix, camera intrinsics
+        fx, fy = self.cam_params["fx"], self.cam_params["fy"]
+        cx, cy = self.cam_params["cx"], self.cam_params["cy"]
         A2 = np.array([ [fx, 0, cx, 0],
                         [0, fy, cy, 0],
                         [0, 0, 1, 0]])
@@ -114,22 +106,25 @@ class SimCam:
         # extrinsics = np.dot(A2, np.dot(T, np.dot(R, A1)))
         # But translate should be first bc it's aligned with the map pixels
         extrinsics = np.dot(A2, np.dot(R, np.dot(T, A1)))
-        inv_extrinsics = np.linalg.inv(extrinsics)
         # print(extrinsics)
+        return extrinsics
 
-        # Start with pixels values of the output image
-        dst_pixels = np.ones((height, width, 3), np.float32)
-        dst_pixels[:, :, :2] = np.mgrid[0:width, 0:height].T
-
-        # TODO: undistorted image -> distorted pixels from full FOV
-
+    def capture(self, uav_pos, uav_att):
+        extrinsics = self.make_extrinsics(uav_pos, uav_att)
+        inv_extrinsics = np.linalg.inv(extrinsics)
+        # Start with pixels values of the distorted output image
+        dst_pixels = self.dst_pixels.copy()
         # Calculate source image x, y
         src_pixels = dst_pixels.dot(inv_extrinsics.T)
         mapx = np.divide(src_pixels[:,:,0], src_pixels[:,:, 2]).astype(np.float32)
         mapy = np.divide(src_pixels[:,:,1], src_pixels[:,:, 2]).astype(np.float32)
         # Remove -z values, points not in the viewing frustum
-        mapx[src_pixels[:,:, 2] < 0] = 0
-        mapy[src_pixels[:,:, 2] < 0] = 0
+        mapx[src_pixels[:,:, 2] < 0] = -1
+        mapy[src_pixels[:,:, 2] < 0] = -1
+        # Remove invalid pixels
+        mapx[self.invalid_pixels] = -1
+        mapy[self.invalid_pixels] = -1
+
         # Use cv2.remap
         out = cv2.remap(self.ground_image, mapx, mapy, cv2.INTER_LINEAR)
 
@@ -178,20 +173,13 @@ class SimCam:
 
 def main():
     # map_filename = "googlemaps-stitch/swx148701-swy196953-nex148704-ney196950-z19.png"
-    # mx, my, zoom = 148701, 196950, 19  # home plate (610, 360)
     # map_filename = "googlemaps-stitch/swx297402-swy393907-nex297409-ney393901-z20.png"
-    # mx, my, zoom = 297402, 393901, 20
     # map_filename = "googlemaps-stitch/swx594804-swy787815-nex594819-ney787803-z21.png"
-    # mx, my, zoom = 594804, 787803, 21  # home plate (2441, 676)
     # map_filename = "googlemaps-stitch/swx1189609-swy1575631-nex1189639-ney1575606-z22.png"
-    # mx, my, zoom = 1189609, 1575606, 22
     # map_filename = "googlemaps-stitch/swx2379218-swy3151263-nex2379279-ney3151213-z23.png"
-    # mx, my, zoom = 2379218, 3151213, 23
 
     map_filename = "googlemaps-stitch/swx297400-swy393909-nex297412-ney393899-z20.png"
-    mx, my, zoom = 297400, 393899, 20
     # map_filename = "googlemaps-stitch/swx594800-swy787819-nex594824-ney787799-z21.png"
-    # mx, my, zoom = 594800, 787799, 21
 
     # lat, lon, alt in meters
     # above home plate
@@ -201,13 +189,7 @@ def main():
     # camera attitude offsets from straight down
     cam_att = [0, 0, 0]
 
-    # this is approximately a gopro hero 3
-    # width, height, in pixels, cam resolution
-    image_size = 1920, 1080
-    # focal_length = 472, 476
-    # optical_center = 960, 540
-    focal_length = 860.42491325/2, 878.34592761/2
-    optical_center = 949.41289961, 498.99907074
+    # calibrate gopro hero 3 1080p wide
     distortion = np.array([
         -2.71969672e-01,    # k1
         1.12979477e-01,     # k2
@@ -215,18 +197,21 @@ def main():
         2.62608080e-04,     # p2
         -2.82170416e-02,    # k3
     ])
+    cam_params = {
+        "width": 1920,
+        "height": 1080,
+        "fx": 860.42491325/2,
+        "fy": 878.34592761/2,
+        "cx": 949.41289961,
+        "cy": 498.99907074,
+        "distortion": distortion,
+    }
 
     sim = SimCam(
-            mx=mx, 
-            my=my, 
-            zoom=zoom, 
             map_filename=map_filename,
+            cam_params=cam_params,
             cam_att=cam_att,
-            image_size=image_size,
-            focal_length=focal_length,
-            optical_center=optical_center,
-            distortion=distortion,
-            )
+        )
 
     out = sim.capture(uav_pos, uav_att)
     cv2.imwrite("SimCam/out0.jpg", out)
